7bd7fc34e72fc58334487732efe68454b1d76d4d
Zitinger <ttpo_100_hagib_3000@mail.ru>
2022-12-06 15:47:18 +0200

    1st day

--- main.lua
+++ main.lua
@@ -10,12 +10,42 @@ game.vx, game.vy = 0, 0;
 
 local bg = display.newImage(game, 'img/first.png', _W/2, _H/2);
 
-bg.xScale, bg.yScale = 0.3, 0.3;
-
-
 local hero = display.newGroup();
 hero.x, hero.y = _W/2, _H/2;
-local body = display.newCircle(hero, 0, 0, 10);
+local body = display.newCircle(hero, 0, 0, 20);
+
+local walls = display.newGroup()
+game:insert(walls)
+
+-- Path for the file to read
+local path = system.pathForFile( "img/first.txt")
+ 
+-- Open the file handle
+local file, errorString = io.open( path, "r" )
+ 
+if not file then
+    -- Error occurred; output the cause
+    print( "File error: " .. errorString )
+else
+    -- Read data from file
+	local n = file:read( "*n" )
+	for i=1, n do
+		x1, y1, x2, y2 = file:read( "*n" ), file:read( "*n" ), file:read( "*n" ), file:read( "*n" )
+		
+		local wall = display.newRect(walls, x1+_W/2, y1, x2-x1 , y2-y1);
+		
+		print(x1, y1, x2, y2)
+		-- table.insert(walls, {x1, y1, x2, y2})
+    end
+    io.close( file )
+end
+
+file = nil
+
+-- for i=0,#walls do
+	-- print(walls[i])
+	-- print(walls[i][0], walls[i][1], walls[i][2], walls[i][3])
+-- end
 
 local function onKeyEvent(event)
 	
@@ -49,6 +79,9 @@ end
 
 Runtime:addEventListener( "key", onKeyEvent )
 Runtime:addEventListener('enterFrame', function()
+	-- if game.x >= 700 or game.x <= 0 then
+		-- game.x = 
+	-- end
 	game.x = game.x + game.vx
 	game.y = game.y + game.vy
 	-- print(hero.x, hero.y)

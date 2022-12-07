1afe60dc69f1c194a722ba58f3cc8042740b47bc
Zitinger <ttpo_100_hagib_3000@mail.ru>
2022-12-07 18:18:02 +0200

    second day final

---
+++ granny.lua
@@ -0,0 +1,5 @@
+local granny = {}
+
+
+
+return granny;
--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ _W = display.contentWidth;
 _H = display.contentHeight;
 
 collision = require('collide')
-emath = require('eliteMath')
 
 local bgbg = display.newRect(_W/2, _H/2, _W, _H);
 bgbg:setFillColor(0.5)
@@ -17,8 +16,10 @@ local hero = display.newGroup();
 hero.x, hero.y = _W/2, _H/2;
 hero.ang = 360;
 local body = display.newCircle(hero, 0, 0, 30);
+body.alpha = 0;
 local face = display.newImage(hero, 'img/main.png', 0, 0)
-face:scale(2.5, 2.5)
+face:scale(2.5,2.5)
+
 
 local walls = display.newGroup()
 game:insert(walls)
@@ -83,22 +84,27 @@ local function onKeyEvent(event)
 end
 
 
-print()
-print()
-print()
-
 Runtime:addEventListener( "key", onKeyEvent )
 Runtime:addEventListener('enterFrame', function()
 	local is_collide_x = false;
 	local is_collide_y = false;
 	
-	-- game.x = game.x + game.vx;
-	-- for i=1,#walls_tb do
-		-- if(collision.isColliding(hero, walls_tb[i], game))then
-			-- is_collide_x = true
-		-- end
-	-- end
-	-- game.x = game.x - game.vx;
+	game.x = game.x + game.vx;
+	for i=1,#walls_tb do
+		if(collision.isColliding(hero, walls_tb[i], game))then
+			is_collide_x = true
+		end
+	end
+	game.x = game.x - game.vx;
+	
+	game.y = game.y + game.vy;
+	for i=1,#walls_tb do
+		if(collision.isColliding(hero, walls_tb[i], game))then
+			is_collide_y = true
+			print(is_collide_y)
+		end
+	end
+	game.y = game.y - game.vy;
 	
 	-- game.y = game.y + game.vy;
 	-- for i=1,#walls_tb do
@@ -109,13 +115,18 @@ Runtime:addEventListener('enterFrame', function()
 	-- game.y = game.x - game.vy;
 	
 	
-	-- if(is_collide_x)then
-		-- game.x = game.x - game.vx
-		-- is_collide_x = false;
-	-- end
+	if(is_collide_x)then
+		game.x = game.x - game.vx
+		is_collide_x = false;
+	end
+	
+	if(is_collide_y)then
+		game.y = game.y - game.vy
+		is_collide_y = false;
+	end
 	-- if(is_collide_y)then
-		-- game.x = game.x - game.vx
-		-- is_collide_x = false;
+		-- game.y = game.y - game.vy
+		-- is_collide_y = false;
 	-- end
 	-- print(is_collide_x, is_collide_y, game.vx, game.vy)
 	game.x = game.x + game.vx
@@ -123,13 +134,13 @@ Runtime:addEventListener('enterFrame', function()
 end)
 
 local function onMouseEvent( event )
-	dx = -hero.x + event.x
-	dy = hero.y - event.y
-	local angle = math.atan2(dx, dy) * 180 / math.pi
-	hero:rotate(360 - hero.ang)
-	hero.ang = angle
-	hero:rotate(hero.ang)
+	dx = -hero.x + event.x;
+	dy = hero.y - event.y;
+	local angle = math.atan2(dx, dy) * 180 / math.pi;
+	hero:rotate(360 - hero.ang);
+	hero.ang = angle;
+	hero:rotate(hero.ang);
 end
                               
 -- Add the mouse event listener.
-Runtime:addEventListener( "mouse", onMouseEvent )
+Runtime:addEventListener("mouse", onMouseEvent)

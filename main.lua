5321d2cf9ee48c1148e1443073b0b355ee05360c
Zitinger <ttpo_100_hagib_3000@mail.ru>
2023-02-26 16:22:53 +0200

    asaa

--- main.lua
+++ main.lua
@@ -1,17 +1,27 @@
 
 collision = require('collide')
 
+_W = display.contentWidth;
+_H = display.contentHeight;
+
+-- TODO: game over src (unit collision); laser collision; unit phase 1 movement
+
 local Unit = {}
-function Unit:new(x, y, game, class)
+function Unit:new(x, y, game, velocity, units_table, class)
 
 	local obj = {}
 		obj.x = x
 		obj.y = y
-		obj.dx = 0
-		obj.dy = 0
+		obj.spawn_x = game.size_of_map/2 - (_W/2) + x
+		obj.spawn_y = game.size_of_map/2 - (_H/2) + y
+		obj.xx = 0
+		obj.yy = 0
 		obj.img = img
 		obj.class = class
+		obj.velocity = velocity
 		obj.phase = 0
+		obj.game = game
+		obj.tb = units_table
 		obj.ang = 360
 		
 	function obj:spawn()
@@ -24,45 +34,70 @@ function Unit:new(x, y, game, class)
 		game:insert(obj.unit)
 	end
 	
+	function obj:kill()
+		
+	end
+	
 	function obj:changePhase(phase)
 		obj.phase = phase
 		print('img/'..obj.phase..'_'..obj.class..'.png')
 		obj.unit.face = display.newImage(obj.unit, 'img/'..obj.phase..'_'..obj.class..'.png')
 	end
 	
-	function obj:move(hero_x, hero_y)
-		
-		ux = game.size_of_map/2 + obj.dx
-		uy = game.size_of_map/2 - obj.y + obj.dy
-		
+	function obj:move()
+
+		ux = obj.spawn_x + obj.xx
+		uy = obj.spawn_y + obj.yy
 		if obj.phase == 0 then
-			print(hero_x, hero_y, ux, uy)
-			dx = obj.unit.x - hero_x;
-			dy = -obj.unit.y + hero_y;
-			local angle = math.atan2(dx, dy) * 180 / math.pi;
-			obj.unit:rotate(360 - obj.ang);
-			obj.ang = angle;
-			obj.unit:rotate(obj.ang);
-			
-			
+			tx = obj.game.size_of_map/2 - game.x
+			ty = obj.game.size_of_map/2 - game.y
 		end
 		if obj.phase == 1 then
-			
-			
-			
-			
+			tx = obj.spawn_x
+			ty = obj.spawn_y
 		end
+		
+		
+		local dx = -ux + tx
+		local dy = uy - ty
+		
+		local angle = math.atan2(dx, dy) * 180 / math.pi;
+		obj.unit:rotate(360 - obj.ang);
+		obj.ang = angle;
+		obj.unit:rotate(obj.ang);
+		
+		dx, dy = tx - ux, ty - uy
+		local vx, vy
+		
+		if dx*dx >= dy*dy then
+			vx = obj.velocity * (dx / math.abs(dx))
+			vy = obj.velocity * (dy / math.abs(dy)) * (math.abs(dy) / math.abs(dx))
+		else
+			vy = obj.velocity * (dy / math.abs(dy))
+			vx = obj.velocity * (dx / math.abs(dx)) * (math.abs(dx) / math.abs(dy))
+		end
+		
+		if dx == 0 then
+			vx = 0
+		end
+		if dy == 0 then
+			vy = 0
+		end
+		
+		-- print(vx, vy)
+		
+		obj.unit.x = obj.unit.x + vx
+		obj.unit.y = obj.unit.y + vy
+		obj.xx = obj.xx + vx
+		obj.yy = obj.yy + vy
+		
+		
 	end
 	
 	setmetatable(obj, self)
     self.__index = self; return obj
 
 end
-
- 
-_W = display.contentWidth;
-_H = display.contentHeight;
-
 
 local bgbg = display.newRect(_W/2, _H/2, _W, _H);
 bgbg:setFillColor(0.5)
@@ -96,10 +131,14 @@ game:insert(walls)
 local walls_tb = {}
 
 
+game.grannys_tb = {}
 
-
-local gr1 = Unit:new(_W/2 + 100, _H/2, game, 'granny')
+local gr1 = Unit:new(_W/2, _H/2 + 100, game, math.random(1, 3), game.grannys_tb, 'granny')
 gr1.spawn()
+local gr2 = Unit:new(_W/2, _H/2 - 100, game, math.random(1, 3), game.grannys_tb, 'granny')
+gr2.spawn()
+
+
 -- gr1:changePhase(1)
 
 
@@ -130,6 +169,20 @@ else
 end
 file = nil
 
+game.is_paused = false
+function game:pause()
+	game.vx, game.vy = 0, 0;
+	game.hero_velocity = 0
+	game.is_paused = true
+end
+
+function game:resume()
+	game.hero_velocity = 5
+	game.is_paused = false
+end
+
+
+
 local function onKeyEvent(event)
 	
 	if (event.keyName == 'd' and event.phase == 'down'  ) then
@@ -159,6 +212,11 @@ local function onKeyEvent(event)
 	
 	if (event.keyName == 'p' and event.phase == 'down'  ) then
 		gr1:changePhase(1)
+		-- game:pause()
+	end
+	if (event.keyName == 'r' and event.phase == 'down'  ) then
+		gr1:changePhase(0)
+		-- game:resume()
 	end
 	
 	
@@ -203,32 +261,35 @@ Runtime:addEventListener("key", onKeyEvent )
 Runtime:addEventListener('enterFrame', function()
 	
 	hero:move()
-	gr1:move(game.size_of_map/2 - game.x, game.size_of_map/2 - game.y)
+	gr1:move()
+	gr2:move()
+	-- gr1:move(hero.x, hero.y)
 	
 end)
 
 local function onMouseEvent( event )
-	dx = -hero.x + event.x;
-	dy = hero.y - event.y;
-	local angle = math.atan2(dx, dy) * 180 / math.pi;
-	hero:rotate(360 - hero.ang);
-	hero.ang = angle;
-	hero:rotate(hero.ang);
+	if not game.is_paused then
+		dx = -hero.x + event.x;
+		dy = hero.y - event.y;
+		local angle = math.atan2(dx, dy) * 180 / math.pi;
+		hero:rotate(360 - hero.ang);
+		hero.ang = angle;
+		hero:rotate(hero.ang);
 	
-	local vx = game.vx
-	local vy = game.vy
-	if event.type == 'down' then
-		if vx == 5 then game.vx = 2 elseif vx == -5 then game.vx = -2 else game.vx = 0 end
-		if vy == 5 then game.vy = 2 elseif vy == -5 then game.vy = -2 else game.vy = 0 end
-		game.hero_velocity = 2
-		laser.alpha = 1
-	end
-	if event.type == 'up' then
-		if vx == 2 then game.vx = 5 elseif vx == -2 then game.vx = -5 else game.vx = 0 end
-		if vy == 2 then game.vy = 5 elseif vy == -2 then game.vy = -5 else game.vy = 0 end
-		game.hero_velocity = 5
-		laser.alpha = 0
+		local vx = game.vx
+		local vy = game.vy
+		if event.type == 'down' then
+			if vx == 5 then game.vx = 2 elseif vx == -5 then game.vx = -2 else game.vx = 0 end
+			if vy == 5 then game.vy = 2 elseif vy == -5 then game.vy = -2 else game.vy = 0 end
+			game.hero_velocity = 2
+			laser.alpha = 1
+		end
+		if event.type == 'up' then
+			if vx == 2 then game.vx = 5 elseif vx == -2 then game.vx = -5 else game.vx = 0 end
+			if vy == 2 then game.vy = 5 elseif vy == -2 then game.vy = -5 else game.vy = 0 end
+			game.hero_velocity = 5
+			laser.alpha = 0
+		end
 	end
 end
-
 Runtime:addEventListener("mouse", onMouseEvent)
